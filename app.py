1fc6695863fc29ef032fa2d1c313b302483b96a2
Ryan Bridge <rbridge@icloud.com>
2019-02-09 20:42:17 -0800

    Apply black formatting
    Signed-off-by: Ryan Bridge <rbridge@icloud.com>

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ import asyncio
 from models import Skill
 from helpers import boot_database
 from dotenv import load_dotenv
+
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN', '')
+TOKEN = os.getenv("DISCORD_TOKEN", "")
 
 client = discord.Client()
 
+
 def clean_content(content, string):
-    return content[len(string):].strip()
+    return content[len(string) :].strip()
+
 
 @client.event
 async def on_message(message):
@@ -20,26 +23,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!skill '):
+    if message.content.startswith("!skill "):
         boot_database()
-        skill_name = clean_content(message.content, '!skill')
-        skill_search = Skill.where('string', 'like', "%{}%".format(skill_name))
+        skill_name = clean_content(message.content, "!skill")
+        skill_search = Skill.where("string", "like", "%{}%".format(skill_name))
         count = skill_search.count()
         if count == 0:
-            await message.channel.send("Couldn't find any skills similar to \"{}\".".format(skill_name))
+            await message.channel.send(
+                'Couldn\'t find any skills similar to "{}".'.format(skill_name)
+            )
         elif count == 1:
             skill = skill_search.first()
-            # await message.channel.send("{}".format(skill.name))
             await message.channel.send("", embed=skill.generateEmbed())
         else:
             skill = skill_search.first()
-            await message.channel.send("{}".format(skill.name))
+            await message.channel.send("", embed=skills.generateEmbed())
+
 
 @client.event
 async def on_ready():
-    print('Logged in as')
+    print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    print('------')
+    print("------")
+
 
 client.run(TOKEN)
--- helpers.py
+++ helpers.py
@@ -1,15 +1,16 @@
 import sqlite3
 from orator import DatabaseManager, Model
 
+
 def boot_database():
     config = {
-        'default': 'sqlite',
-        'sqlite': {
-            'driver': 'sqlite',
-            'database': 'divinity.db',
-            'prefix': '',
-            'log_queries': True,
-        }
+        "default": "sqlite",
+        "sqlite": {
+            "driver": "sqlite",
+            "database": "divinity.db",
+            "prefix": "",
+            "log_queries": True,
+        },
     }
 
     db = DatabaseManager(config)
